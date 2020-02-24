72a96da9e67a5bdaf47567ca0a09144917151f08
Default User <defaultuser@Defaults-MacBook-Pro.local>
2020-02-24 21:16:37 +0000

    second push - added JS to check if cards match

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-let cardOne = 'king';
-let cardTwo = 'king';
-let cardThree = 'Queen';
-let cardFour = 'Queen';
+
+let cards = ['king', 'king', 'queen', 'queen'];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
 console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert('matched congratulations!')
+    }
+    else {
+        alert('Unlucky but nope!')
+    }
+}
