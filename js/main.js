db84a30dfa3b636a7894d2a9ad333667ab97c8e0
Default User <defaultuser@Defaults-MacBook-Pro.local>
2020-02-25 07:59:17 +0000

    added click events and further logic

--- js/main.js
+++ js/main.js
@@ -33,18 +33,28 @@ function checkForMatch(){
     }
   
 
-function flipCard(cardId){
+  function flipCard(){
 
+    let cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
+    console.log(cardId);
     console.log('User flipped over ' + cards[cardId].rank + ' of ' + cards[cardId].suit);
     console.log(cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
-
 
 if (cardsInPlay.length === 2) {
     checkForMatch();
 }
 }
 
-flipCard(2);
-flipCard(0);
+function createBoard(){
+    for(let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', '/images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
 
+createBoard();
