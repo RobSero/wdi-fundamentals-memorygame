adb30579ae7c38ac15a3165f2503cc6e06b27990
Default User <defaultuser@Defaults-MacBook-Pro.local>
2020-02-25 07:32:48 +0000

    added card objects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 
-let cards = ['king', 'king', 'queen', 'queen'];
+let cards = [{
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+},
+{
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+},
+{
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+},
+{
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+}
+];
 let cardsInPlay = [];
 
 function checkForMatch(){
@@ -15,8 +35,10 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-    console.log('User flipped over ' + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+    console.log('User flipped over ' + cards[cardId].rank + ' of ' + cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
+
 
 if (cardsInPlay.length === 2) {
     checkForMatch();
