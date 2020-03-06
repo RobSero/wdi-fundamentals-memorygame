47a244874bc1ebdc056a23f0f75398d31071ab25
Default User <defaultuser@Defaults-MacBook-Pro.local>
2020-03-06 14:18:11 +0000

    added addtional features: random card generator, scorestreak, transitions

--- js/main.js
+++ js/main.js
@@ -21,14 +21,24 @@ let cards = [{
 }
 ];
 let cardsInPlay = [];
+let currentScore = 0;
+randomArrayIndex = [];
 
 function checkForMatch(){
  
         if (cardsInPlay[0] === cardsInPlay[1]){
-            alert('matched congratulations!')
+            document.querySelector('h1').innerText = 'CONGRATULATIONS!!';
+            document.querySelector('header').style.backgroundColor = 'rgb(207, 241, 10)';
+            resetButton.removeAttribute('id');
+            currentScore = currentScore + 1;
+            document.querySelector('#score').innerHTML = currentScore;
         }
         else {
-            alert('Unlucky but nope!')
+            document.querySelector('h1').innerText = 'GAME OVER!!';
+            document.querySelector('header').style.backgroundColor = 'rgb(187, 49, 241)';
+            resetButton.removeAttribute('id');
+            currentScore = 0;
+            document.querySelector('#score').innerHTML = currentScore;
         }
     }
   
@@ -48,13 +58,58 @@ if (cardsInPlay.length === 2) {
 }
 
 function createBoard(){
+    randomIndex();
     for(let i = 0; i < cards.length; i++){
         let cardElement = document.createElement('img');
         cardElement.setAttribute('src', '/images/back.png');
-        cardElement.setAttribute('data-id', i);
+        cardElement.setAttribute('data-id', randomArrayIndex[i]);
+        cardElement.setAttribute('class','card');
         cardElement.addEventListener('click', flipCard);
         document.getElementById('game-board').appendChild(cardElement);
     }
 }
 
 createBoard();
+
+let resetButton = document.querySelector('#reset-button');
+let instructions = document.querySelector('p');
+let howToPlayButton = document.querySelector('#how-to-play');
+
+howToPlayButton.addEventListener('click', function(){
+    if(instructions.getAttribute('class') === 'hidden'){
+        instructions.removeAttribute('class','hidden')
+    }
+    else {
+        instructions.setAttribute('class','hidden')
+    }
+});
+
+resetButton.addEventListener('click', resetGame);
+
+
+function resetGame(){
+    resetButton.setAttribute('id','reset-button');
+    document.querySelector('h1').innerText = 'Memory Card Game';
+    document.querySelector('header').style.backgroundColor = '#F15B31';
+    cardsInPlay = [];
+    document.getElementById('game-board').innerHTML = '';
+    randomArrayIndex = [];
+    
+    createBoard();
+}
+
+
+
+
+function randomIndex(){
+  while (randomArrayIndex.length < 4){
+      let num = randomNumber();
+      if(!randomArrayIndex.includes(num)){
+          randomArrayIndex.push(num)
+      }
+  }
+}
+
+function randomNumber(){
+    return Math.floor(Math.random()*cards.length);
+}
